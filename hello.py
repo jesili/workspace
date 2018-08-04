20efd8f7123318230fa442b1eecb74cc2a4d011b
jesili <1402376141@qq.com>
2018-08-04 16:51:34 +0800

    hello world

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print('hello, world!')
+print("please tell me your name, enter 'q' to quit" )
+while True:
+    name = input("what's your name? ")
+    if name != 'q':
+        print("hello, " + name)
+    else:
+        break
